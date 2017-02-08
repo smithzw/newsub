52002249dd2be66be72e06bfd32355efd24ba35d
smithzw <smith_zw@126.com>
2017-02-08 15:19:32 +0800

    1
    2

---
+++ CrawlApplication1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.Security.Permissions;
+namespace CrawlApplication1
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]//　　注意： 类定义前需要加上下面两行，否则调用失败！
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 38);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(735, 190);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 234);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(735, 82);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "InvokeMember";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "InvokeScript";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(672, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.Location = new System.Drawing.Point(83, 11);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(664, 21);
+            this.UrlTextBox.TabIndex = 5;
+            this.UrlTextBox.Text = "http://www.dmzj.com/shengdoushi/15177.shtml";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "网站地址：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 435);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UrlTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CrawlApplication1/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using mshtml;
+using System.Reflection;
+using System.Collections;
+
+namespace CrawlApplication1
+{
+   public partial class Form1 : Form
+    {
+       private int n = 0;
+       private bool bFirst = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HtmlElement head = webBrowser1.Document.GetElementsByTagName("head")[0];
+            HtmlElement scriptEl = webBrowser1.Document.CreateElement("script");
+            IHTMLScriptElement element = (IHTMLScriptElement)scriptEl.DomElement;
+            element.text = "function sayHello() { var objs = new Array(4);  alert(__HOST__.img_prefix); var b = arr_pages.join(\"|\"); return b; }";//var b = objs.join(\"-\");
+            //IHTMLScriptElement el;
+
+            head.AppendChild(scriptEl);
+            ////webBrowser1.Document.InvokeScript("sayHello");
+            ////string jCode = "alert(\"HelloHelloHelloHello\");";
+            //// or any combination of your JavaScript commands
+            //// (including function calls, variables... etc)
+            //IHTMLDocument2 aa;
+            //// WebBrowser webBrowser1 is what you are using for your web browser
+            ////webBrowser1.Document.InvokeScript("eval", new object[] { jCode });
+            //object[] objs = new object[4];
+            //object obj = webBrowser1.Document.InvokeScript("jsAlert", new String[] { "info1", "info2" });
+            //MessageBox.Show(objs.ToString());
+            IHTMLDocument2 vDocument = webBrowser1.Document.DomDocument as IHTMLDocument2;
+            IHTMLWindow2 vWindow = vDocument.parentWindow;
+            Type vWindowType = vWindow.GetType();
+            ArrayList alist = new ArrayList();
+            string[] strs = new string[4];
+            //object xpt = vWindowType.InvokeMember("_xpt", BindingFlags.GetProperty, null, vWindow, new object[] { });
+
+            object getarr =  webBrowser1.Document.InvokeScript("sayHello", new object[] { });
+            MessageBox.Show(getarr.ToString());
+        }
+        //public event WebBrowserDocumentCompletedEventHandler DocumentCompleted;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser.DocumentTitle 属性值更改时发生。
+        //public event EventHandler DocumentTitleChanged;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser 控件导航到或离开使用了加密的网站时发生。
+        //public event EventHandler EncryptionLevelChanged;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser 控件下载文件时发生。
+        //public event EventHandler FileDownload;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser 控件导航到新文档并开始加载该文档时发生。
+        //public event WebBrowserNavigatedEventHandler Navigated;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser 控件导航到新文档之前发生。
+        //public event WebBrowserNavigatingEventHandler Navigating;
+        ////
+        //// 摘要:
+        ////     在浏览器新窗口打开之前发生。
+        //public event CancelEventHandler NewWindow;
+        ////
+        //// 摘要:
+        ////     当 System.Windows.Forms.WebBrowser.Padding 属性的值更改时发生。
+        //public event EventHandler PaddingChanged;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser 控件已更新有关要导航到的文档的下载进度的信息时发生。
+        //public event WebBrowserProgressChangedEventHandler ProgressChanged;
+        ////
+        //// 摘要:
+        ////     在 System.Windows.Forms.WebBrowser.StatusText 属性值更改时发生。
+        //public event EventHandler StatusTextChanged;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //FileInfo file = new FileInfo("index.html");
+            //FileInfo file = new FileInfo("http://www.dmzj.com/shengdoushi/15177.shtml");
+            //webBrowser1.Url = new Uri(file.FullName);
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            //     在 System.Windows.Forms.WebBrowser.DocumentTitle 属性值更改时发生。
+            webBrowser1.DocumentTitleChanged += new EventHandler(webBrowser1_DocumentTitleChanged);
+            //     在 System.Windows.Forms.WebBrowser 控件导航到或离开使用了加密的网站时发生。
+            webBrowser1.EncryptionLevelChanged += new EventHandler(webBrowser1_EncryptionLevelChanged);
+            //     在 System.Windows.Forms.WebBrowser 控件下载文件时发生。
+            webBrowser1.FileDownload += new EventHandler(webBrowser1_FileDownload);
+                 //在 System.Windows.Forms.WebBrowser 控件导航到新文档并开始加载该文档时发生。
+            webBrowser1.Navigated += new WebBrowserNavigatedEventHandler(webBrowser1_Navigated);
+            //     在 System.Windows.Forms.WebBrowser 控件导航到新文档之前发生。
+            webBrowser1.Navigating += new WebBrowserNavigatingEventHandler(webBrowser1_Navigating);
+            //     在浏览器新窗口打开之前发生。
+            webBrowser1.NewWindow += new CancelEventHandler(webBrowser1_NewWindow);
+            //     在 System.Windows.Forms.WebBrowser.StatusText 属性值更改时发生。
+            webBrowser1.StatusTextChanged +=new EventHandler(webBrowser1_StatusTextChanged);
+            //     当 System.Windows.Forms.WebBrowser.Padding 属性的值更改时发生。
+            webBrowser1.ProgressChanged +=new WebBrowserProgressChangedEventHandler(webBrowser1_ProgressChanged);
+
+            webBrowser1.ParentChanged += new EventHandler(webBrowser1_ParentChanged);
+            webBrowser1.PreviewKeyDown += new PreviewKeyDownEventHandler(webBrowser1_PreviewKeyDown);
+            //     在 System.Windows.Forms.WebBrowser 控件已更新有关要导航到的文档的下载进度的信息时发生。
+            webBrowser1.ProgressChanged += new WebBrowserProgressChangedEventHandler(webBrowser1_ProgressChanged);
+
+            webBrowser1.Navigate("http://www.dmzj.com/shengdoushi/15177.shtml");
+            //webBrowser1.ObjectForScripting = this;
+        }
+        
+        void  webBrowser1_StatusTextChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("StatusTextChanged");
+        }
+        void webBrowser1_FileDownload(object sender, EventArgs e)
+        {
+            //MessageBox.Show("FileDownload");
+        }
+        void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            //MessageBox.Show("ProgressChanged");
+        }
+        void webBrowser1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            //MessageBox.Show("PreviewKeyDown");
+        }
+        void webBrowser1_ParentChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("ParentChanged");
+        }
+        void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            //MessageBox.Show("NewWindow");
+        }
+        void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            //MessageBox.Show("Navigating");
+        }
+        void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            //MessageBox.Show("Navigated");
+        }
+        void webBrowser1_EncryptionLevelChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("EncryptionLevelChanged");
+        }
+        void webBrowser1_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("DocumentTitleChanged");
+        }
+        void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlElement head = webBrowser1.Document.GetElementsByTagName("head")[0];
+            if (head == null) return;
+            HtmlElement scriptEl = webBrowser1.Document.CreateElement("script");
+            if (scriptEl == null) return;
+            IHTMLScriptElement element = (IHTMLScriptElement)scriptEl.DomElement;
+            if (element == null) return;
+            element.text = "function sayHello() { var objs = new Array(4);  alert(__HOST__.img_prefix); var b = arr_pages.join(\"|\"); return b; }";//var b = objs.join(\"-\");
+
+            HtmlElement e1 = head.AppendChild(scriptEl);
+            
+            if (e1 == null) return;
+            //IHTMLDocument2 vDocument = webBrowser1.Document.DomDocument as IHTMLDocument2;
+            //IHTMLWindow2 vWindow = vDocument.parentWindow;
+            //Type vWindowType = vWindow.GetType();
+            //ArrayList alist = new ArrayList();
+            ////string[] strs = new string[4];
+            
+            if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+            {
+                if (bFirst)
+                {
+                    bFirst = false;
+                    return;
+                }
+                //n++;
+                object getarr = webBrowser1.Document.InvokeScript("sayHello", new object[] { });
+                MessageBox.Show(getarr.ToString());
+            }
+            //if (n < 2) return;
+            //n = 0;
+
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //webBrowser1.ResetText();
+            webBrowser1.Navigate(UrlTextBox.Text.Trim());
+        }
+    }
+}
